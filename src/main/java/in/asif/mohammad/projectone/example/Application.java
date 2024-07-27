487dcc6c4fb93ca45896333fc85780269f759aa2
mohammadasif2808 <mohammadasif2808@gmail.com>
2024-07-27 19:14:45 +0530

    initial commit

---
+++ src/main/java/in/asif/mohammad/projectone/example/Application.java
@@ -0,0 +1,26 @@
+package in.asif.mohammad.projectone.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/api")
+class HelloController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello, World!";
+	}
+}
