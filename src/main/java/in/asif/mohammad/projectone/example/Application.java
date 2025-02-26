0b8b7ef697780f0e4cfbf8acf968cdf564dbd7f8
mohammadasif2808 <mohammadasif2808@gmail.com>
2025-02-26 13:48:55 +0530

    added cloudformation script + one api

--- src/main/java/in/asif/mohammad/projectone/example/Application.java
+++ src/main/java/in/asif/mohammad/projectone/example/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -21,6 +22,11 @@ class HelloController {
 
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello, World!";
+		return "Hello, World! How Are You?";
+	}
+
+	@GetMapping("/greet")
+	public String greetUser(@RequestParam(defaultValue = "Guest") String name) {
+		return "Hello, " + name + "! Welcome to our API.";
 	}
 }
